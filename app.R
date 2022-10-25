6ad500adbfcf5ff4767cb1cb53e5ea82b0d05eff
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-10-25 12:14:51 +1000

    run styler::style_dir()

--- app.R
+++ app.R
@@ -13,10 +13,10 @@ library(shinyjs)
 source("utils.R")
 
 cutpoint_methods <- c(
-  "Cost-effective", 
-  "The Closest to (0, 1) Criteria", 
-  "Youden", 
-  "Sens-Spec product", 
+  "Cost-effective",
+  "The Closest to (0, 1) Criteria",
+  "Youden",
+  "Sens-Spec product",
   "Index of Union"
 )
 
@@ -28,61 +28,66 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("cutpoint",
-                  "Select cutpoint:",
-                  min = 0,
-                  max = 1,
-                  value = 0.5),
+        "Select cutpoint:",
+        min = 0,
+        max = 1,
+        value = 0.5
+      ),
       tags$br(),
       dropdownButton(
-        checkboxInput('cutpoints_all', 'Select All/None', value=FALSE),
-        label="Add cutpoints", status="default", width=dropdown_width,
+        checkboxInput("cutpoints_all", "Select All/None", value = FALSE),
+        label = "Add cutpoints", status = "default", width = dropdown_width,
         checkboxGroupInput(
-          inputId="cutpoint_methods", label="Cutpoint methods:", width=dropdown_width,
-          choices=cutpoint_methods,
-          selected=NULL)
-        ),
+          inputId = "cutpoint_methods", label = "Cutpoint methods:", width = dropdown_width,
+          choices = cutpoint_methods,
+          selected = NULL
+        )
+      ),
       tags$br(),
       bsCollapsePanel(
         "Inputs values of Net Monetary Benefit (NMB):",
         splitLayout(
-          numericInput("tp_nmb", "True Positive", value=NULL),
-          numericInput("fp_nmb", "False Positive", value=NULL)
+          numericInput("tp_nmb", "True Positive", value = NULL),
+          numericInput("fp_nmb", "False Positive", value = NULL)
         ),
         splitLayout(
-          numericInput("fn_nmb", "False Negative", value=NULL),
-          numericInput("tn_nmb", "True Negative", value=NULL)
+          numericInput("fn_nmb", "False Negative", value = NULL),
+          numericInput("tn_nmb", "True Negative", value = NULL)
         )
       ),
       tags$br(),
       radioButtons(
         "data_source",
-        label="Use simulated or your own data:",
-        choices=c("simulated", "upload"),
-        selected="simulated"
+        label = "Use simulated or your own data:",
+        choices = c("simulated", "upload"),
+        selected = "simulated"
       ),
       bsCollapse(
-        id="sampling_controls",
+        id = "sampling_controls",
         bsCollapsePanel(
           "Settings for sampling",
           sliderInput("event_rate",
-                      "Rate of the outcome:",
-                      min = 0,
-                      max = 0.5,
-                      value = 0.1),
+            "Rate of the outcome:",
+            min = 0,
+            max = 0.5,
+            value = 0.1
+          ),
           sliderInput("auc",
-                      "Model discrimination (AUC):",
-                      min = 0.5,
-                      max = 1,
-                      value = 0.75),
+            "Model discrimination (AUC):",
+            min = 0.5,
+            max = 1,
+            value = 0.75
+          ),
           sliderInput("n_samples",
-                      "Sample size:",
-                      min = 0,
-                      max = 1e4,
-                      value = 1e3)
+            "Sample size:",
+            min = 0,
+            max = 1e4,
+            value = 1e3
+          )
         )
       ),
       fileInput(
-        "datafile", "Upload CSV file", 
+        "datafile", "Upload CSV file",
         accept = c(
           "text/csv",
           "text/comma-separated-values,text/plain",
@@ -93,18 +98,17 @@ ui <- fluidPage(
       uiOutput("actual_selector")
     ),
     mainPanel(
-      plotOutput("plots", height=320),
+      plotOutput("plots", height = 320),
       tags$br(),
-      tableOutput("confusion_matrix") 
+      tableOutput("confusion_matrix")
     )
   )
 )
 
 
 server <- function(input, output, session) {
-  
   observe({
-    if(input$data_source == "simulated"){
+    if (input$data_source == "simulated") {
       show("sampling_controls")
       hide("datafile")
     } else {
@@ -112,7 +116,7 @@ server <- function(input, output, session) {
       show("datafile")
     }
   })
-  
+
   filedata <- reactive({
     infile <- input$datafile
     if (is.null(infile)) {
@@ -120,7 +124,7 @@ server <- function(input, output, session) {
     }
     read.csv(infile$datapath)
   })
-  
+
   cols <- reactive({
     df <- filedata()
     if (is.null(df)) {
@@ -129,192 +133,209 @@ server <- function(input, output, session) {
       return(names(df))
     }
   })
-  
+
   output$proba_selector <- renderUI({
     if (is.null(cols())) {
       return(NULL)
     }
     selectInput("col_proba", "Select the predicted probability variable from:", cols())
   })
-  
+
   output$actual_selector <- renderUI({
     if (is.null(cols())) {
       return(NULL)
     }
     selectInput("col_actual", "Select the (binary) outcome variable from:", cols())
   })
-  
+
   observe({
     updateCheckboxGroupInput(
-      inputId='cutpoint_methods', 
-      choices=cutpoint_methods,
-      selected=if(input$cutpoints_all) cutpoint_methods else c()
+      inputId = "cutpoint_methods",
+      choices = cutpoint_methods,
+      selected = if (input$cutpoints_all) cutpoint_methods else c()
     )
   })
-  
+
   observe({
-    if(all(!is.na(input$tp_nmb), !is.na(input$fp_nmb), !is.na(input$fn_nmb), !is.na(input$tn_nmb))){
+    if (all(!is.na(input$tp_nmb), !is.na(input$fp_nmb), !is.na(input$fn_nmb), !is.na(input$tn_nmb))) {
       return() # do nothing if all inputs are there
-    } else if("Cost-effective" %in% input$cutpoint_methods){
+    } else if ("Cost-effective" %in% input$cutpoint_methods) {
       updateCheckboxGroupInput( # update checkbox to uncheck the cost-effective box
-        inputId='cutpoint_methods', 
-        choices=cutpoint_methods,
-        selected=input$cutpoint_methods[input$cutpoint_methods != "Cost-effective"]
+        inputId = "cutpoint_methods",
+        choices = cutpoint_methods,
+        selected = input$cutpoint_methods[input$cutpoint_methods != "Cost-effective"]
       )
       shinyalert( # show alert to user
-        "Oops!", 
+        "Oops!",
         paste(
           "The Cost-effective threshold only works when there are valid",
           "inputs for the Net Monetary Benefit 2x2 matrix below.",
           " You can expand it by clicking on the box below."
-        ), 
-        type = "error")
+        ),
+        type = "error"
+      )
     }
   })
-  
+
   df_preds <- reactive({
-    
-    if(input$data_source=="simulated"){
+    if (input$data_source == "simulated") {
       set.seed(42)
       data <- get_sample(
-        auc=input$auc, 
-        n_samples=input$n_samples, 
-        prevalence=input$event_rate
+        auc = input$auc,
+        n_samples = input$n_samples,
+        prevalence = input$event_rate
       )
-      
-      mod <- glm(actual ~ predicted, data=data, family=binomial())
+
+      mod <- glm(actual ~ predicted, data = data, family = binomial())
       data$actual <- as.factor(data$actual)
-      data$proba <- predict(mod, type="response")
-      
+      data$proba <- predict(mod, type = "response")
     } else {
-      if(is.null(filedata())) return()
-      if(length(unique(c(input$col_proba, input$col_actual)))!=2) return()
+      if (is.null(filedata())) {
+        return()
+      }
+      if (length(unique(c(input$col_proba, input$col_actual))) != 2) {
+        return()
+      }
       data <-
-        filedata()  %>% 
-        select(proba=input$col_proba, actual=input$col_actual) %>%
-        mutate(actual=as.factor(actual))
-      
-      if(length(unique(data$actual))!=2) return()
+        filedata() %>%
+        select(proba = input$col_proba, actual = input$col_actual) %>%
+        mutate(actual = as.factor(actual))
+
+      if (length(unique(data$actual)) != 2) {
+        return()
+      }
     }
-    if(is.null(data)) return()
-    if(nrow(data)==0) return()
+    if (is.null(data)) {
+      return()
+    }
+    if (nrow(data) == 0) {
+      return()
+    }
     data
   })
-  
+
   get_cutpoints <- reactive({
     thresholds <- get_thresholds(
-      predicted=df_preds()$proba, 
-      actual=df_preds()$actual,
+      predicted = df_preds()$proba,
+      actual = df_preds()$actual,
       NMB = c(
-        "TP"=input$tp_nmb,
-        "TN"=input$tn_nmb,
-        "FP"=input$fp_nmb,
-        "FN"=input$fn_nmb
+        "TP" = input$tp_nmb,
+        "TN" = input$tn_nmb,
+        "FP" = input$fp_nmb,
+        "FN" = input$fn_nmb
       ),
-      get_what=c("optimal_cutpoint", "sensitivity", "specificity")
+      get_what = c("optimal_cutpoint", "sensitivity", "specificity")
     )
   })
-  
+
   output$plots <- renderPlot({
-    if(is.null(df_preds())) return()
-    
+    if (is.null(df_preds())) {
+      return()
+    }
+
     theme_list <- list(
       theme_bw(),
       theme(text = element_text(size = 13))
     )
-    
+
     pal <- "Dark2"
-    
-    p_hist <- 
+
+    p_hist <-
       df_preds() %>%
-      ggplot(aes(x=proba, fill=actual)) + 
+      ggplot(aes(x = proba, fill = actual)) +
       geom_histogram() +
       labs(
-        x="Predicted probabilities",
-        y="Number of observations",
-        fill="Event"
+        x = "Predicted probabilities",
+        y = "Number of observations",
+        fill = "Event"
       ) +
-      scale_x_continuous(limits=c(0,1)) +
+      scale_x_continuous(limits = c(0, 1)) +
       theme_list
-    
+
     cutpoints_df <- get_cutpoints()
     cutpoints_df <- filter(cutpoints_df, cutpoint_method %in% input$cutpoint_methods)
     cutpoints_df <- rbind(
       cutpoints_df,
-      data.frame(optimal_cutpoint=input$cutpoint,
-                 sensitivity=get_selected_cp_metrics()$sensitivity,
-                 specificity=get_selected_cp_metrics()$specificity,
-                 cutpoint_method=c("Selected cutpoint"))
+      data.frame(
+        optimal_cutpoint = input$cutpoint,
+        sensitivity = get_selected_cp_metrics()$sensitivity,
+        specificity = get_selected_cp_metrics()$specificity,
+        cutpoint_method = c("Selected cutpoint")
+      )
     ) %>%
-      mutate(cutpoint_method = factor(cutpoint_method, levels=c("Selected cutpoint", cutpoint_methods)))
-    
-    cutpoints_df$cp_size <- seq(from=4, to=3, length.out=nrow(cutpoints_df))
-    
-    
+      mutate(cutpoint_method = factor(cutpoint_method, levels = c("Selected cutpoint", cutpoint_methods)))
+
+    cutpoints_df$cp_size <- seq(from = 4, to = 3, length.out = nrow(cutpoints_df))
+
+
     rocobj <- pROC::roc(df_preds()$actual, df_preds()$proba)
     p_roc <- pROC::ggroc(rocobj) +
       labs(
-        col="",
-        y="Specificity",
-        x="Sensitivity"
+        col = "",
+        y = "Specificity",
+        x = "Sensitivity"
       ) +
       theme_list
-    
+
     # add cutpoints to graphs
-    p_hist <- 
+    p_hist <-
       p_hist +
       geom_vline(
-        data=cutpoints_df, 
-        aes(xintercept=optimal_cutpoint, col=cutpoint_method),
-        size=cutpoints_df$cp_size,
-        alpha=0.7, 
-        linetype="dashed"
+        data = cutpoints_df,
+        aes(xintercept = optimal_cutpoint, col = cutpoint_method),
+        size = cutpoints_df$cp_size,
+        alpha = 0.7,
+        linetype = "dashed"
       ) +
-      scale_color_brewer(palette=pal)
-    
-    p_roc <- 
+      scale_color_brewer(palette = pal)
+
+    p_roc <-
       p_roc +
       geom_point(
-        data=cutpoints_df, 
-        aes(x=sensitivity, y=specificity, col=cutpoint_method),
-        alpha=0.7
+        data = cutpoints_df,
+        aes(x = sensitivity, y = specificity, col = cutpoint_method),
+        alpha = 0.7
       ) +
       geom_label_repel(
-        data=cutpoints_df, 
-        aes(x=sensitivity, y=specificity, label=cutpoint_method)
+        data = cutpoints_df,
+        aes(x = sensitivity, y = specificity, label = cutpoint_method)
       ) +
-      scale_color_brewer(palette=pal) +
-      guides(col="none") 
-    
+      scale_color_brewer(palette = pal) +
+      guides(col = "none")
+
     # get legends out of plots and put at bottom with cowplot
     leg1 <- get_legend(
       p_hist +
-        guides(col = "none",
-               fill = guide_legend(nrow=1)) +
-        theme(legend.position="bottom")
+        guides(
+          col = "none",
+          fill = guide_legend(nrow = 1)
+        ) +
+        theme(legend.position = "bottom")
     )
-    
+
     leg2 <- get_legend(
       p_roc +
-        guides(col=guide_legend(nrow=1)) +
-        theme(legend.position="bottom")
+        guides(col = guide_legend(nrow = 1)) +
+        theme(legend.position = "bottom")
     )
-    
-    p_hist <- p_hist + guides(col="none", fill="none")
-    p_roc <- p_roc + guides(col="none")
-    
+
+    p_hist <- p_hist + guides(col = "none", fill = "none")
+    p_roc <- p_roc + guides(col = "none")
+
     plots <- plot_grid(p_hist, p_roc)
-    legends <- plot_grid(leg1, leg2, ncol=1)
-    plot_grid(plots, legends, ncol=1, rel_heights = c(0.8,0.2))
+    legends <- plot_grid(leg1, leg2, ncol = 1)
+    plot_grid(plots, legends, ncol = 1, rel_heights = c(0.8, 0.2))
   })
-  
+
   get_confusion <- reactive({
-    cm <- 
+    cm <-
       df_preds() %>%
-      group_by(actual, predicted=proba > input$cutpoint) %>%
-      summarize(n=n()) %>% 
+      group_by(actual, predicted = proba > input$cutpoint) %>%
+      summarize(n = n()) %>%
       ungroup()
-    if(nrow(cm) != 4) {
+    
+    # if the cutpoint is above or below ALL predicted probs, add the empty rows to confusion matrix
+    if (nrow(cm) != 4) { 
       cm <- rbind(
         cm,
         data.frame(
@@ -326,59 +347,61 @@ server <- function(input, output, session) {
     }
     cm
   })
-  
+
   output$confusion_matrix <- renderText({
-    if(is.null(df_preds())) return()
+    if (is.null(df_preds())) {
+      return()
+    }
     get_confusion() %>%
-      mutate(predicted = factor(predicted, levels=c(FALSE, TRUE))) %>%
+      mutate(predicted = factor(predicted, levels = c(FALSE, TRUE))) %>%
       pivot_wider(
-        names_from=predicted, values_from=n, 
-        names_expand=T, values_fill=0
+        names_from = predicted, values_from = n,
+        names_expand = T, values_fill = 0
       ) %>%
-      mutate(actual=as.logical(as.numeric(actual)-1)) %>%
-      rename(" "=actual) %>%
+      mutate(actual = as.logical(as.numeric(actual) - 1)) %>%
+      rename(" " = actual) %>%
       kable(
-        "html", 
-        caption=glue::glue(
+        "html",
+        caption = glue::glue(
           "Confusion matrix using visualised data above ",
           "and the selected cutpoint ({input$cutpoint})"
         )
       ) %>%
-      kable_paper(full_width=FALSE, font_size=18) %>%
+      kable_paper(full_width = FALSE, font_size = 18) %>%
       column_spec(2:3, width = "6em") %>%
-      column_spec(1, bold=T) %>%
-      add_header_above(c(" "=1, "Predicted"=2)) %>%
+      column_spec(1, bold = T) %>%
+      add_header_above(c(" " = 1, "Predicted" = 2)) %>%
       group_rows("Actual", 1, 2)
   })
-  
+
   get_selected_cp_metrics <- reactive({
     cp_fp <-
       get_confusion() %>%
-      filter(predicted==1, actual==0) %>%
+      filter(predicted == 1, actual == 0) %>%
       pull(n)
 
     cp_fn <-
       get_confusion() %>%
-      filter(predicted==0, actual==1) %>%
+      filter(predicted == 0, actual == 1) %>%
       pull(n)
 
     cp_tp <-
       get_confusion() %>%
-      filter(predicted==1, actual==1) %>%
+      filter(predicted == 1, actual == 1) %>%
       pull(n)
 
     cp_tn <-
       get_confusion() %>%
-      filter(predicted==0, actual==0) %>%
+      filter(predicted == 0, actual == 0) %>%
       pull(n)
 
     list(
-      fp=cp_fp,
-      fn=cp_fn,
-      tp=cp_tp,
-      tn=cp_tn,
-      sensitivity=sensitivity(tp=cp_tp, fn=cp_fn),
-      specificity=specificity(fp=cp_fp, tn=cp_tn)
+      fp = cp_fp,
+      fn = cp_fn,
+      tp = cp_tp,
+      tn = cp_tn,
+      sensitivity = sensitivity(tp = cp_tp, fn = cp_fn),
+      specificity = specificity(fp = cp_fp, tn = cp_tn)
     )
   })
 }
--- utils.R
+++ utils.R
@@ -1,21 +1,21 @@
-get_sample <- function(auc, n_samples, prevalence, scale_to_d=F){
+get_sample <- function(auc, n_samples, prevalence, scale_to_d = F) {
   # https://stats.stackexchange.com/questions/422926/generate-synthetic-data-given-auc
   # http://dx.doi.org/10.5093/ejpalc2018a5
-  t <- sqrt(log(1/(1-auc)**2))
-  z <- t-((2.515517 + 0.802853*t + 0.0103328*t**2) /
-            (1 + 1.432788*t + 0.189269*t**2 + 0.001308*t**3))
-  d <- z*sqrt(2)
-  
-  n_pos <- sum(sample(c(0,1), n_samples, replace=TRUE, prob=c(1-prevalence, prevalence)))
+  t <- sqrt(log(1 / (1 - auc)**2))
+  z <- t - ((2.515517 + 0.802853 * t + 0.0103328 * t**2) /
+    (1 + 1.432788 * t + 0.189269 * t**2 + 0.001308 * t**3))
+  d <- z * sqrt(2)
+
+  n_pos <- sum(sample(c(0, 1), n_samples, replace = TRUE, prob = c(1 - prevalence, prevalence)))
   n_neg <- n_samples - n_pos
-  
-  x <- c(rnorm(n_neg, mean=0), rnorm(n_pos, mean=d))
+
+  x <- c(rnorm(n_neg, mean = 0), rnorm(n_pos, mean = d))
   y <- c(rep(0, n_neg), rep(1, n_pos))
-  
-  if(scale_to_d){
-    x <- x/d
+
+  if (scale_to_d) {
+    x <- x / d
   }
-  return(data.frame(predicted=x, actual=y))
+  return(data.frame(predicted = x, actual = y))
 }
 
 dropdownButton <- function(label = "", status = c("default", "primary", "success", "info", "warning", "danger"), ..., width = NULL) {
@@ -24,14 +24,15 @@ dropdownButton <- function(label = "", status = c("default", "primary", "success
   # dropdown button content
   html_ul <- list(
     class = "dropdown-menu",
-    style = if (!is.null(width)) 
-      paste0("width: ", validateCssUnit(width), ";"),
+    style = if (!is.null(width)) {
+      paste0("width: ", validateCssUnit(width), ";")
+    },
     lapply(X = list(...), FUN = tags$li, style = "margin-left: 10px; margin-right: 10px;")
   )
   # dropdown button apparence
   html_button <- list(
-    class = paste0("btn btn-", status," dropdown-toggle"),
-    type = "button", 
+    class = paste0("btn btn-", status, " dropdown-toggle"),
+    type = "button",
     `data-toggle` = "dropdown"
   )
   html_button <- c(html_button, list(label))
@@ -44,7 +45,8 @@ dropdownButton <- function(label = "", status = c("default", "primary", "success
     tags$script(
       "$('.dropdown-menu').click(function(e) {
       e.stopPropagation();
-});")
+});"
+    )
   )
 }
 
@@ -58,48 +60,47 @@ roc_iu <- function(tp, fp, tn, fn, .roc_curve, ...) {
   return(iu)
 }
 
-fx_total_nmb <- function(tn, tp, fn, fp, utility_tp, utility_tn, cost_fp, cost_fn, ...){
+fx_total_nmb <- function(tn, tp, fn, fp, utility_tp, utility_tn, cost_fp, cost_fn, ...) {
   total_nmb <- utility_tp * tp + utility_tn * tn + cost_fp * fp + cost_fn * fn
   total_nmb <- matrix(total_nmb, ncol = 1)
   colnames(total_nmb) <- "total_nmb"
   total_nmb
 }
 
-get_thresholds <- function(predicted, actual, NMB, get_what=c("optimal_cutpoint")){
-
+get_thresholds <- function(predicted, actual, NMB, get_what = c("optimal_cutpoint")) {
   pt_er <- cutpointr(
-    x=predicted, class=actual, method=minimize_metric, metric=roc01,
-    silent=TRUE
+    x = predicted, class = actual, method = minimize_metric, metric = roc01,
+    silent = TRUE
   )[, get_what]
 
-  
+
   pt_youden <- cutpointr(
-    x=predicted, class=actual, method=maximize_metric, metric=youden,
-    silent=TRUE
+    x = predicted, class = actual, method = maximize_metric, metric = youden,
+    silent = TRUE
   )[, get_what]
-  
-  if(all(!is.na(NMB))){
+
+  if (all(!is.na(NMB))) {
     pt_cost_effective <- cutpointr(
-      x=predicted, class=actual, method=maximize_metric, metric=fx_total_nmb,
-      utility_tp=NMB["TP"], utility_tn=NMB["TN"],
-      cost_fp=NMB["FP"], cost_fn=NMB["FN"],
-      silent=TRUE
+      x = predicted, class = actual, method = maximize_metric, metric = fx_total_nmb,
+      utility_tp = NMB["TP"], utility_tn = NMB["TN"],
+      cost_fp = NMB["FP"], cost_fn = NMB["FN"],
+      silent = TRUE
     )[, get_what]
   } else {
     pt_cost_effective <- NA
   }
-  
+
   pt_cz <- cutpointr(
-    x=predicted, class=actual, method=maximize_metric, metric=prod_sens_spec,
-    silent=TRUE
+    x = predicted, class = actual, method = maximize_metric, metric = prod_sens_spec,
+    silent = TRUE
   )[, get_what]
-  
+
   pt_iu <- cutpointr(
-    x=predicted, class=actual, method=minimize_metric, metric=roc_iu,
-    silent=TRUE
+    x = predicted, class = actual, method = minimize_metric, metric = roc_iu,
+    silent = TRUE
   )[, get_what]
-  
-  res <- 
+
+  res <-
     rbind(
       pt_cost_effective,
       pt_er,
@@ -107,15 +108,14 @@ get_thresholds <- function(predicted, actual, NMB, get_what=c("optimal_cutpoint"
       pt_cz,
       pt_iu
     )
-  
+
   res$cutpoint_method <- c(
-    "Cost-effective", 
-    "The Closest to (0, 1) Criteria", 
-    "Youden", 
-    "Sens-Spec product", 
+    "Cost-effective",
+    "The Closest to (0, 1) Criteria",
+    "Youden",
+    "Sens-Spec product",
     "Index of Union"
   )
-  
+
   res
 }
-
